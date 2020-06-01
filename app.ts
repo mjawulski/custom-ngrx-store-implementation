536ed7bcd75f0449345fc139bed763b74e3eb1ab
Michał Jawulski <mjawulski@gmail.com>
2020-06-01 14:31:25 +0200

    Added store

--- app.ts
+++ app.ts
@@ -7,6 +7,12 @@ import * as fromRender from './utils/render';
   const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
   const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+  const store = new fromStore.Store(
+    {},
+    { todos: [{ label: 'Learn NGRX', complete: false }] }
+  );
+
+  console.log('State::', store.value);
   button.addEventListener(
     'click',
     () => {
--- store/store.ts
+++ store/store.ts
@@ -1,1 +1,15 @@
-export class Store {}
+export class Store {
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
+  constructor(
+    reducers: { [key: string]: Function },
+    initialState: { [key: string]: any }
+  ) {
+    this.reducers = reducers;
+    this.state = initialState;
+  }
+
+  get value() {
+    return this.state;
+  }
+}
